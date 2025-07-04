af38e00a27214980f4e26eb4bceda20630f672b9
Amal PK <105988204+0xKrat0s@users.noreply.github.com>
2025-07-04 11:26:40 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true, // Optional but helps with routing on GitHub Pages
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
